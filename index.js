dc17e48ae0e330bbf1949962c66c062c8b55ff83
Algirdas Sidabras <algirdas@mediapark.com>
2018-11-25 17:14:01 +0200

    adds requests logger

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const restify = require('restify');
+const fs = require('fs');
+const util = require('util');
+const moment = require('moment');
 
 function respond(req, res, next) {
-    console.log(req.body);
-    res.send('Success');
+    const stream = fs.createWriteStream(`${__dirname}/logs/requests.log`, {flags: 'a+'});
+    stream.write(`[${moment().format('YYYY-MM-DD hh:mm:ss')}] ${util.format.apply(null, [req.body])}\n`);
+    res.send({success: true});
     next();
 }
 
 const server = restify.createServer();
 
-server.use(restify.plugins.bodyParser);
+server.use(restify.plugins.bodyParser());
 
-server.post('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.post('/workpackages', respond);
 
-server.listen(8080, function() {
+server.listen(8899, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
